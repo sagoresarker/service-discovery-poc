a0452cc84c8b0279adb5b37d044abc1c3b18e03d
sagoresarker <sagoresarker.cse@gmail.com>
2024-10-19 19:36:19 +0600

    read host ip from env

--- vm-agent/go.mod
+++ vm-agent/go.mod
@@ -2,4 +2,7 @@ module github.com/sagoresarker/svc-discovery-vm-poc
 
 go 1.22
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+)
--- vm-agent/internal/config/config.go
+++ vm-agent/internal/config/config.go
@@ -1,13 +1,40 @@
 package config
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
 	HostServiceURL string
 	AgentPort      int
 }
 
 func LoadConfig() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("error loading .env file: %v", err)
+	}
+
+	hostIP := os.Getenv("HOST_IP")
+	if hostIP == "" {
+		return nil, fmt.Errorf("HOST_IP environment variable is not set")
+	}
+
+	hostPort := os.Getenv("HOST_PORT")
+	if hostPort == "" {
+		hostPort = "8092" // Default port if not specified
+	}
+
+	agentPort := os.Getenv("AGENT_PORT")
+	if agentPort == "" {
+		agentPort = "8093" // Default port if not specified
+	}
+
 	return &Config{
-		HostServiceURL: "http://localhost:8092",
+		HostServiceURL: fmt.Sprintf("http://%s:%s", hostIP, hostPort),
 		AgentPort:      8093,
 	}, nil
 }
